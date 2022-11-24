14cfaacb7dda2c9f98e16d38db19cea334a2c7d8
amtuazon4 <amtuazon4@up.edu.ph>
2022-11-24 10:34:45 +0800

    feat: added ya rly in the grab_lexeme.py

--- grab_lexeme.py
+++ grab_lexeme.py
@@ -438,6 +438,18 @@ def get_lexemes(lexemeArr, lines, line, index):
         new = line[num_exact:len(line)]
         lines.remove(line)
         lines.insert(index, new.strip())
+    
+    elif(len(re.findall("^YA RLY", line)) != 0):
+        lexeme_info = []
+        lexeme_info.append("YA RLY")
+        lexeme_info.append("keyword")
+        lexemeArr.append(lexeme_info)
+        
+        num_exact = len("YA RLY")
+        new = line[num_exact:len(line)]
+        lines.remove(line)
+        lines.insert(index, new.strip())
+    
 
     elif(len(re.findall("^NO WAI", line)) != 0):
         lexeme_info = []
@@ -743,16 +755,50 @@ HAI
 3.4
 KTHXBYE"""
 
-y = """I HAS OBTW
-OBTW this
+y = """HAI
+
+	I HAS A choice
+	I HAS A input
+
+	BTW if w/o MEBBE, 1 only, everything else is invalid
+	VISIBLE "1. Compute age"
+	VISIBLE "2. Compute tip"
+	VISIBLE "3. Compute square area"
+	VISIBLE "0. Exit"
+
+	VISIBLE "Choice: "
+	GIMMEH choice
+
+	BOTH SAEM choice AN 1
+	O RLY?
+		YA RLY
+			VISIBLE "Enter birth year: "
+			GIMMEH input
+			VISIBLE DIFF OF 2022 AN input
 OBTW
-OBTW
-ok 
-nani
-watashi wa
-bruhTLDR
-aTLDR
-TLDRA
-I HAS A TLDR
-TLDR"""
+	BTW uncomment this portion if you have MEBBE
+	BTW else, this portion should be ignored
+
+		MEBBE BOTH SAEM choice AN 2
+			VISIBLE "Enter bill cost: "
+			GIMMEH input
+			VISIBLE "Tip: " PRODUCKT OF input AN 0.1
+		MEBBE BOTH SAEM choice AN 3
+			VISIBLE "Enter width: "
+			GIMMEH input
+			VISIBLE "Square Area: " PRODUCKT OF input AN input
+		MEBBE BOTH SAEM choice AN 0
+			VISIBLE "Goodbye"
+TLDR
+		NO WAI
+			VISIBLE "Invalid Input!"
+	OIC
+
+	DIFFRINT BIGGR OF 3 AN choice AN 3
+	O RLY?
+		YA RLY
+			VISIBLE "Invalid input is > 3."
+	OIC
+
+KTHXBYE"""
 lex_analyze(y)
