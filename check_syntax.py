93fda19f4b6a3bee04791082ae0a8149190fc3b5
amtuazon4 <amtuazon4@up.edu.ph>
2022-11-25 23:20:06 +0800

    fix: changed all elif -> if

--- check_syntax.py
+++ check_syntax.py
@@ -30,123 +30,128 @@ def check_syntax(lexemeArr):
                 del testing_list[index]
                 testing_list.insert(index, "literal")
                 change = True
-            elif(i[1] == "String Delimiter" and (index+2) < len(testing_list)):
+            if(i[1] == "String Delimiter" and (index+2) < len(testing_list)):
                 if(testing_list[index+1] == "literal" and testing_list[index+2][1] == "String Delimiter"):
                     del testing_list[index:(index+3)]
                     testing_list.insert(index, "literal")
                     change = True
 
             #Variable Identifier
-            elif(i[1] == "Variable Identifier"):    
+            if(i[1] == "Variable Identifier"):    
                 del testing_list[index]
                 testing_list.insert(index, "varident")
                 change = True
 
             #Operations
-            elif(i[0] in ["SUM OF", "DIFF OF", "PRODUKT OF", "QUOSHUNT OF", "MOD OF", "BIGGR OF", "SMALLR OF", "BOTH SAEM", "DIFFRINT"] and (index+3)<len(testing_list)):
+            if(i[0] in ["SUM OF", "DIFF OF", "PRODUKT OF", "QUOSHUNT OF", "MOD OF", "BIGGR OF", "SMALLR OF", "BOTH SAEM", "DIFFRINT"] and (index+3)<len(testing_list)):
                 if(testing_list[index+1] in ["literal", "varident", "expr"] and testing_list[index+2][0] == "AN" and testing_list[index+3] in ["literal", "varident", "expr"]):
                     del testing_list[index:(index+4)]
                     testing_list.insert(index, "expr")
                     change = True
 
             #Print statement
-            elif(i[0] == "VISIBLE" and (index+1)<len(testing_list)):
+            if(i[0] == "VISIBLE" and (index+1)<len(testing_list)):
                 if(testing_list[index+1] in ["expr", "literal", "varident"]):
                     del testing_list[index:(index+2)]
                     testing_list.insert(index, "expr")
                     change = True
                 
             #Input statement
-            elif(i[0] == "GIMMEH" and (index+1)<len(testing_list)):
+            if(i[0] == "GIMMEH" and (index+1)<len(testing_list)):
                 if(testing_list[index+1] == "varident"):
                     del testing_list[index:(index+2)]
                     testing_list.insert(index, "expr")
                     change = True
 
             #Variable assignment
-            elif(i == "varident" and (index+2)<len(testing_list)):
+            if(i == "varident" and (index+2)<len(testing_list)):
                 if(testing_list[index+1][0] == "R" and testing_list[index+2] in ["literal", "varident", "expr"]):
                     del testing_list[index:(index+3)]
                     testing_list.insert(index, "expr")
                     change = True
 
             #Variable initialization 
-            elif(i[0] == "I HAS A" and (index+2)<len(testing_list)): # ***
+            if(i[0] == "I HAS A" and (index+2)<len(testing_list)): # ***
                 if(testing_list[index+1] == "varident" and testing_list[index+2][0] != "ITZ"):
                     del testing_list[index:(index+2)]
                     testing_list.insert(index, "varinit")
                     change = True
-            elif(i[0] == "I HAS A" and (index+4)<len(testing_list)):
+            if(i[0] == "I HAS A" and (index+1)==(len(testing_list)-1)): # ***
+                if(testing_list[index+1] == "varident"):
+                    del testing_list[index:(index+2)]
+                    testing_list.insert(index, "varinit")
+                    change = True
+            if(i[0] == "I HAS A" and (index+4)<len(testing_list)):
                 if(testing_list[index+1] == "varident" and testing_list[index+2][0] == "ITZ" and testing_list[index+3] in ["literal", "expr"]):
                     del testing_list[index:(index+4)]
                     testing_list.insert(index, "varinit")
                     change = True
 
             # Boolean operations
-            elif(i[0] in ["BOTH OF", "EITHER OF", "WON OF"] and (index+3)<len(testing_list)):
+            if(i[0] in ["BOTH OF", "EITHER OF", "WON OF"] and (index+3)<len(testing_list)):
                 if(testing_list[index+1] in ["literal", "varident", "expr"] and testing_list[index+2][0] == "AN" and testing_list[index+3] in ["literal", "varident", "expr"]):
                     del testing_list[index:(index+4)]
                     testing_list.insert(index, "boolop")
                     change = True
-            elif(i[0] == "NOT" and (index+1)<len(testing_list)):
+            if(i[0] == "NOT" and (index+1)<len(testing_list)):
                 if(testing_list[index+1] in ["literal", "varident", "expr"]):
                     del testing_list[index:(index+2)]
                     testing_list.insert(index, "boolop")
                     change = True
-            elif(i == "boolop" and (index+3)<len(testing_list)):
+            if(i == "boolop" and (index+3)<len(testing_list)):
                 if(testing_list[index+1][0] == "AN" and testing_list[index+2] in ["boolop", "infstatement"] and testing_list[index+3][0] == "MKAY"):
                     del testing_list[index:(index+3)]
                     testing_list.insert(index, "infstatement")
                     change = True
-            elif(i[0] in ["ANY OF", "ALL OF"] and (index+2)<len(testing_list)):
+            if(i[0] in ["ANY OF", "ALL OF"] and (index+2)<len(testing_list)):
                 if(testing_list[index+1] == "infstatement" and testing_list[index+2][0] == "MKAY"):
                     del testing_list[index:(index+3)]
                     testing_list.insert(index, "expr")
                     change = True
 
             #Sting concatenation
-            elif(i[0] == "SMOOSH" and index+3<len(testing_list)):
+            if(i[0] == "SMOOSH" and index+3<len(testing_list)):
                 if(testing_list[index+1] in ["literal", "varident", "concats"] and testing_list[index+2][0] == "AN" and testing_list[index+3] in ["literal", "varident"]):
                     del testing_list[(index+1):(index+4)]
                     testing_list.insert((index+1), "concats")
                     change = True
 
             #Typecasting
-            elif(i[0] == "MAEK" and (index+3)<len(testing_list)):
+            if(i[0] == "MAEK" and (index+3)<len(testing_list)):
                 if(testing_list[index+1] == "varident" and testing_list[index+2][0] == "A" and testing_list[index+3][1] == "TYPE Literal"):
                     del testing_list[index:(index+4)]
                     testing_list.insert(index, "expr")
                     change = True
-            elif(i[0] == "MAEK" and (index+2)<len(testing_list)):
+            if(i[0] == "MAEK" and (index+2)<len(testing_list)):
                 if(testing_list[index+1] == "varident" and testing_list[index+2][1] == "TYPE Literal"):
                     del testing_list[index:(index+3)]
                     testing_list.insert(index, "expr")
                     change = True
 
             #if-else
-            elif(i[0] == "YA RLY" and (index+2)<len(testing_list)):
+            if(i[0] == "YA RLY" and (index+2)<len(testing_list)):
                 if(testing_list[index+1] in ["expr", "ifelseStatement"] and testing_list[index+2] == "expr"):
                     del testing_list[(index+1):(index+3)]
                     testing_list.insert((index+1), "ifelseStatement")
                     change = True
-            elif(i[0] == "YA RLY" and (index+2)<len(testing_list)):
+            if(i[0] == "YA RLY" and (index+2)<len(testing_list)):
                 if(testing_list[index+1] == "expr" and testing_list[index+2][0] in ["OIC", "NO WAI"]):
                     del testing_list[index+1]
                     testing_list.insert((index+1), "ifelseStatement")
                     change = True
-            elif(i[0] == "NO WAI" and (index+2)<len(testing_list)):
+            if(i[0] == "NO WAI" and (index+2)<len(testing_list)):
                 if(testing_list[index+1] == "expr" and testing_list[index+2][0] == "OIC"):
                     del testing_list[index+1]
                     testing_list.insert((index+1), "ifelseStatement")
                     change = True
             
-            elif(i[0] == "O RLY?" and (index+3)<len(testing_list)):
+            if(i[0] == "O RLY?" and (index+3)<len(testing_list)):
                 if(testing_list[index+1][0] == "YA RLY" and testing_list[index+2] == "ifelseStatement" and testing_list[index+3][0] == "OIC"):
                     del testing_list[index:(index+4)]
                     testing_list.insert(index, "ifelse")
                     change = True
             
-            elif(i[0] == "O RLY?" and (index+5)<len(testing_list)):
+            if(i[0] == "O RLY?" and (index+5)<len(testing_list)):
                 if(testing_list[index+1][0] == "YA RLY" and testing_list[index+2] == "ifelseStatement" and testing_list[index+3][0] == "NO WAI" and testing_list[index+4] == "ifelseStatement" and testing_list[index+5][0] == "OIC"):
                     del testing_list[index:(index+6)]
                     testing_list.insert(index, "ifelse")
@@ -157,12 +162,12 @@ def check_syntax(lexemeArr):
             
 
             #Comment
-            elif(i[0] == "OBTW" or i[1] == "comment" or i[0] == "TLDR" or i[0] == "BTW"):
+            if(i[0] == "OBTW" or i[1] == "comment" or i[0] == "TLDR" or i[0] == "BTW"):
                 del testing_list[index]
                 change = True
 
             #Invalid
-            elif(i[1] == "Invalid"):
+            if(i[1] == "Invalid"):
                 print("Invalid Syntax")
                 return(False)
             index += 1
@@ -217,16 +222,16 @@ def check_syntax(lexemeArr):
                     del testing_list[index:(index+2)]
                     testing_list.insert(index, "statement")
                     change=True
-            elif(i in ["varinit", "statement2"]):
+            if(i in ["varinit", "statement2"]):
                 del testing_list[index]
                 testing_list.insert(index, "statement")
                 change = True
-            elif(i in ["print", "input", "varassign", "ifelse", "expr", "case", "loop"]):
+            if(i in ["print", "input", "varassign", "ifelse", "expr", "case", "loop"]):
                 del testing_list[index]
                 testing_list.insert(index, "statement2")
                 change = True
             
-            elif(i[0] == "HAI" and len(testing_list) == 3):
+            if(i[0] == "HAI" and len(testing_list) == 3):
                 if(testing_list[index+1] == "statement" and testing_list[index+2][0] == "KTHXBYE"):
                     del testing_list[index:(index+3)]
                     testing_list.insert(index, "program")
@@ -357,6 +362,5 @@ SUM OF x AN Y
 SUM OF x AN Y
 SUM OF x AN Y
 SUM OF x AN Y
-I HAS A MONDE
-"""
-lex_analyze(lexemeArr, e)
+I HAS A MONDE"""
+lex_analyze(lexemeArr, d)
