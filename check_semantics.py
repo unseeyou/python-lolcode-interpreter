72b61a914d2dff161bcbb651c2ab3668dc2351b3
Coleen Agsao <caagsao@up.edu.ph>
2022-11-27 02:19:12 +0800

    feat: replace identifier if existent in symbol table

--- check_semantics.py
+++ check_semantics.py
@@ -5,6 +5,26 @@ lexemeArr = []
 symbolTable = []
 
 
+def grab_identifiers(symbolTable):
+    identifiers = []
+
+    for i in symbolTable:
+        identifiers.append(i[0])
+
+    return identifiers
+
+
+# function that checks if identifier already exists. If not, append. If existing, update the symbol table
+def insertToTable(testing_list, symbolTable, index, new_identifier, new_value):
+    if new_identifier not in grab_identifiers(symbolTable):
+        symbolTable.append([testing_list[index+1][0], new_value])
+    else:
+        dupIndex = grab_identifiers(symbolTable).index(
+            testing_list[index+1][0])
+        del symbolTable[dupIndex]
+        symbolTable.insert(dupIndex, [testing_list[index+1][0], new_value])
+
+
 def grab_symbol_table(lexemeArr):
     testing_list = []
 
@@ -18,23 +38,26 @@ def grab_symbol_table(lexemeArr):
             if testing_list[index+1][1] == "Variable Identifier":
                 if (index + 2) < len(testing_list):
                     if testing_list[index+2][0] != "ITZ":
-                        symbolTable.append([testing_list[index+1][0], ""])
+                        insertToTable(testing_list, symbolTable,
+                                      index, testing_list[index+1][0], "")
                 elif (index + 2) == len(testing_list):
-                    symbolTable.append([testing_list[index+1][0], ""])
+                    insertToTable(testing_list, symbolTable,
+                                  index, testing_list[index+1][0], "")
+
         # Variable initialization (Case 1.1: Literal is NUMBR, NUMBAR, TROOF)
         if (i[0] == "I HAS A" and (index+3) < len(testing_list)):
             if (testing_list[index+1][1] == "Variable Identifier" and testing_list[index+2][0] == "ITZ"):
                 if testing_list[index+3][1] in ["NUMBR Literal", "NUMBAR Literal", "TROOF Literal"]:
                     #del testing_list[index:(index+2)]
-                    symbolTable.append(
-                        [testing_list[index+1][0], testing_list[index+3][0]])
+                    insertToTable(testing_list, symbolTable,
+                                  index, testing_list[index+1][0], testing_list[index+3][0])
         # Variable initialization (Case 1.2: YARN Literal)
         if (i[0] == "I HAS A" and (index+5) < len(testing_list)):
             if (testing_list[index+1][1] == "Variable Identifier" and testing_list[index+2][0] == "ITZ"):
                 # Case 1.2: Literal is YARN
                 if testing_list[index+3][1] == "String Delimiter" and testing_list[index+4][1] == "YARN Literal" and testing_list[index+5][1] == "String Delimiter":
-                    symbolTable.append(
-                        [testing_list[index+1][0], testing_list[index+4][0]])
+                    insertToTable(testing_list, symbolTable, index,
+                                  testing_list[index+1][0], testing_list[index+4][0])
 
     for i in symbolTable:
         print(i)
@@ -64,6 +87,7 @@ k = "HAI I HAS A monde KTHXBYE"
 g = """HAI
     I HAS A monde
     I HAS A num ITZ 17
+    I HAS A num ITZ 18
     I HAS A name ITZ "seventeen"
     I HAS A fnum ITZ 17.0
     I HAS A flag ITZ WIN
@@ -80,4 +104,4 @@ h = """HAI
 KTHXBYE
 """
 
-#lex_analyze(lexemeArr, k)
+lex_analyze(lexemeArr, g)
